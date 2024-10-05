d50969134a6490ac148a89c1a0a6924b299bccba
Igor Goroshit <igor@webt.co.il>
2024-10-05 12:02:19 +0300

    remove some filters

--- src/Filters/LessphpFilter.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Igorgoroshit\Pipeline\Filters;
-
-use Assetic\Asset\AssetInterface;
-use Assetic\Filter\FilterInterface;
-
-class LessphpFilter extends \Assetic\Filter\LessphpFilter
-{
-
-}
