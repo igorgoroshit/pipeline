e15260d76cc9841a172f19b1fbaaf664d5557ede
Igor Goroshit <igor@webt.co.il>
2024-10-14 15:06:05 +0300

    add Less filter

---
+++ src/Filters/LessphpFilter.php
@@ -0,0 +1,9 @@
+<?php namespace Igorgoroshit\Pipeline\Filters;
+
+use Assetic\Asset\AssetInterface;
+use Assetic\Filter\FilterInterface;
+
+class LessphpFilter extends \Assetic\Filter\LessphpFilter
+{
+
+}
